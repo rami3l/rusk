245a7a2e9da6ca05ae35887379447e519ce3ad8a
Gen Li <597974733.qq@gmail.com>
2019-08-20 17:11:15 +0800

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,32 @@ use std::collections::{HashMap, VecDeque};
 use std::rc::Rc;
 // use std::cell::RefCell;
 
-// Types
+#[macro_use]
+extern crate lazy_static;
+
+macro_rules! apply {
+    // def apply(func, args): return func(args)
+    ($func:expr, $args: expr)  => {
+        $func($args)
+    };
+}
+
+// * Types
+
 #[derive(Clone)]
-enum Exp {
+enum Exp<'a> {
     Bool(bool),
     Symbol(String),
     Number(f64),         // ! int unimplemented
-    List(VecDeque<Exp>), // also used as AST
-    Lambda(ScmLambda),
-    Primitive(fn(&[Exp]) -> Option<Exp>),
-}
-
-#[derive(Clone)]
-struct ScmLambda {
-    params: Rc<Exp>,
-    body: Rc<Exp>,
+    List(VecDeque<Exp<'a>>), // also used as AST
+    Closure(ScmClosure<'a>),
+    Primitive(fn(&[Exp<'a>]) -> Option<Exp<'a>>),
+    Empty,
 }
 
 #[derive(Clone)]
 struct Env<'a> {
-    data: HashMap<String, Exp>,
+    data: HashMap<String, Exp<'a>>,
     outer: Option<&'a Env<'a>>,
 }
 
@@ -32,7 +38,7 @@ impl<'a> Env<'a> {
             outer,
         }
     }
-    fn find(&self, symbol: &Exp) -> Option<&Env> {
+    fn find(&self, symbol: &Exp) -> Option<&Env<'a>> {
         // find the innermost Env where symbol appears
         match symbol {
             Exp::Symbol(s) => match self.data.get(s) {
@@ -46,7 +52,7 @@ impl<'a> Env<'a> {
         }
     }
 
-    fn lookup(&self, symbol: &Exp) -> Option<Exp> {
+    fn lookup(&self, symbol: &Exp) -> Option<Exp<'a>> {
         match symbol {
             Exp::Symbol(s) => match self.data.get(s) {
                 Some(exp) => Some(exp.clone()),
@@ -57,8 +63,9 @@ impl<'a> Env<'a> {
     }
 }
 
+#[derive(Clone)]
 struct ScmClosure<'a> {
-    body: Rc<Exp>,
+    body: Rc<Exp<'a>>,
     env: Env<'a>,
 }
 
@@ -106,6 +113,8 @@ fn cons(x: &Exp, y: &Vec<Exp>) -> Option<Exp> {
 
 // ! Oh, apply, unpacking...
 */
+
+// * Parsing
 
 fn tokenize(str_exp: &str) -> VecDeque<String> {
     let res: VecDeque<_> = str_exp
@@ -162,6 +171,109 @@ fn gen_ast(tokens: &mut VecDeque<String>) -> Result<Exp, ScmErr> {
 
 fn parse(str_exp: &str) -> Result<Exp, ScmErr> {
     gen_ast(&mut tokenize(str_exp))
+}
+
+
+// * Primitive operators
+
+// All primitive operators are fn(&[Exp]) -> Option<Exp>
+// in order to fit into Exp::Primitive(fn(&[Exp]) -> Option<Exp>)
+
+fn add<'a>(pair: &[Exp<'a>]) -> Option<Exp<'a>> {
+    match pair {
+        &[Exp::Number(a), Exp::Number(b)] => Some(Exp::Number(a+b)),
+        _ => None,
+    }
+}
+
+fn sub<'a>(pair: &[Exp<'a>]) -> Option<Exp<'a>> {
+    match pair {
+        &[Exp::Number(a), Exp::Number(b)] => Some(Exp::Number(a-b)),
+        _ => None,
+    }
+}
+
+fn mul<'a>(pair: &[Exp<'a>]) -> Option<Exp<'a>> {
+    match pair {
+        &[Exp::Number(a), Exp::Number(b)] => Some(Exp::Number(a*b)),
+        _ => None,
+    }
+}
+
+fn div<'a>(pair: &[Exp<'a>]) -> Option<Exp<'a>> {
+    match pair {
+        &[Exp::Number(a), Exp::Number(b)] => Some(Exp::Number(a-b)),
+        _ => None,
+    }
+}
+
+// * Prelude
+
+fn get_prelude() -> Env<'static> {
+    let mut res = Env::new(None);
+    let data = &mut res.data;
+
+    // initializing the environment
+    data.insert(String::from("+"), Exp::Primitive(add));
+    data.insert(String::from("-"), Exp::Primitive(sub));
+    data.insert(String::from("*"), Exp::Primitive(mul));
+    data.insert(String::from("/"), Exp::Primitive(div));
+
+    res
+}
+
+lazy_static! {
+    static ref GLOBAL_ENV: Env<'static> = get_prelude();
+}
+
+
+fn eval<'a>(exp: Exp<'a>, env: Env<'a>) -> Result<Exp<'a>, ScmErr> {
+    match exp {
+        Exp::Number(_) => Ok(exp),
+        Exp::Symbol(_) => {
+            match env.lookup(&exp) {
+                Some(res) => Ok(res),
+                None => Err(ScmErr::from("Symbol undefined")),
+                // TODO: detailed info
+            }
+        }
+        Exp::List(list) => {
+            if let Exp::Symbol(head) = &list[0] {
+            match head.as_ref() {
+                "quote" => Ok(list[1]),
+
+                "lambda" => {
+                    let closure_env = Env::new(Some(&env));
+                    let closure = ScmClosure {
+                        body: Rc::new(exp),
+                        env: closure_env,
+                    };
+                    Ok(Exp::Closure(closure))
+                }
+
+                "define" => {
+                    let Exp::Symbol(symbol) = &list[1]; // ! unpacking
+                    let definition = &list[2];
+                    env.data.insert(*symbol, eval(*definition, env).unwrap());
+                    println!("Symbol defined");
+                    // TODO: detailed info
+                    Ok(Exp::Empty)
+                }
+
+                "if" => {
+                    let condition = &list[1];
+                    let then_ = &list[2];
+                    let else_ = &list[3];
+                    // return eval(then_ if eval(condition, env) else else_, env)
+                }
+
+                _ => Err(ScmErr::from("Unknown keyword")),
+            }}
+            else {
+                Err(ScmErr::from("Expected a Symbol head for a List"))
+            }
+        }
+    }
 }
 
 fn main() {
