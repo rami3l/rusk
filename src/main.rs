65ed7ae566604ad360893268f942a065cdc4dcc4
Gen Li <597974733.qq@gmail.com>
2019-08-30 09:20:43 +0800

    refactor test code, preparing to refactor Env.

--- src/main.rs
+++ src/main.rs
@@ -606,112 +606,121 @@ mod tests {
     #[test]
     fn test_plus() {
         let mut env: Env = get_prelude();
-        check_io_str("(+ 1 2)", "Ok(3)", &mut env);
+        let env = &mut env;
+        check_io_str("(+ 1 2)", "Ok(3)", env);
     }
 
     #[test]
     fn test_plus_nested() {
         let mut env: Env = get_prelude();
-        check_io_str("(+ 1 (* 2 3))", "Ok(7)", &mut env);
+        let env = &mut env;
+        check_io_str("(+ 1 (* 2 3))", "Ok(7)", env);
     }
 
     #[test]
     fn test_define_val() {
         let mut env: Env = get_prelude();
-        check_io_str("(define x 3)", "Ok()", &mut env);
-        check_io_str("x", "Ok(3)", &mut env);
-        check_io_str("(+ x 1)", "Ok(4)", &mut env);
+        let env = &mut env;
+        check_io_str("(define x 3)", "Ok()", env);
+        check_io_str("x", "Ok(3)", env);
+        check_io_str("(+ x 1)", "Ok(4)", env);
     }
 
     #[test]
     fn test_define_proc_basic() {
         let mut env: Env = get_prelude();
-        check_io_str("(define x 3)", "Ok()", &mut env);
-        check_io_str("x", "Ok(3)", &mut env);
-        check_io_str("(define one (lambda () 1))", "Ok()", &mut env);
-        check_io_str("(one)", "Ok(1)", &mut env);
-        check_io_str("(+ (one) (+ 2 x))", "Ok(6)", &mut env);
+        let env = &mut env;
+        check_io_str("(define x 3)", "Ok()", env);
+        check_io_str("x", "Ok(3)", env);
+        check_io_str("(define one (lambda () 1))", "Ok()", env);
+        check_io_str("(one)", "Ok(1)", env);
+        check_io_str("(+ (one) (+ 2 x))", "Ok(6)", env);
     }
 
     #[test]
     fn test_define_proc_call_prim() {
         let mut env: Env = get_prelude();
-        check_io_str("(define x 3)", "Ok()", &mut env);
-        check_io_str("x", "Ok(3)", &mut env);
-        check_io_str("(define inc (lambda (x) (+ x 1)))", "Ok()", &mut env);
-        check_io_str("(inc 100)", "Ok(101)", &mut env);
-        check_io_str("(inc x)", "Ok(4)", &mut env);
+        let env = &mut env;
+        check_io_str("(define x 3)", "Ok()", env);
+        check_io_str("x", "Ok(3)", env);
+        check_io_str("(define inc (lambda (x) (+ x 1)))", "Ok()", env);
+        check_io_str("(inc 100)", "Ok(101)", env);
+        check_io_str("(inc x)", "Ok(4)", env);
     }
 
     #[test]
     fn test_cond() {
         let mut env: Env = get_prelude();
-        check_io_str("(if #t 123 wtf)", "Ok(123)", &mut env);
-        check_io_str("(if #f wtf 123)", "Ok(123)", &mut env);
+        let env = &mut env;
+        check_io_str("(if #t 123 wtf)", "Ok(123)", env);
+        check_io_str("(if #f wtf 123)", "Ok(123)", env);
         check_io_str(
             "(cond (#f wtf0) (#f wtf1) (#t 456) (else wtf3))",
             "Ok(456)",
-            &mut env,
+            env,
         );
         check_io_str(
             "(cond (#f wtf0) (#f wtf1) (#f wtf2) (else 789))",
             "Ok(789)",
-            &mut env,
+            env,
         );
     }
 
     #[test]
     fn test_eq() {
         let mut env: Env = get_prelude();
-        check_io_str("(define one (lambda () 1))", "Ok()", &mut env);
-        check_io_str("(= 1 1)", "Ok(true)", &mut env);
-        check_io_str("(= 1 (one))", "Ok(true)", &mut env);
-        check_io_str("(if (= 1 (one)) 123 wtf)", "Ok(123)", &mut env);
-        check_io_str("(if (= (one) (+ 4 5)) wtf 123)", "Ok(123)", &mut env);
+        let env = &mut env;
+        check_io_str("(define one (lambda () 1))", "Ok()", env);
+        check_io_str("(= 1 1)", "Ok(true)", env);
+        check_io_str("(= 1 (one))", "Ok(true)", env);
+        check_io_str("(if (= 1 (one)) 123 wtf)", "Ok(123)", env);
+        check_io_str("(if (= (one) (+ 4 5)) wtf 123)", "Ok(123)", env);
     }
 
     #[test]
     fn test_cons() {
         let mut env: Env = get_prelude();
-        check_io_str("(car (cons 123 456))", "Ok(123)", &mut env);
-        check_io_str("(cdr (cons 123 456))", "Ok(456)", &mut env);
-        check_io_str("(define p (cons (cons 1 2) (cons 3 4)))", "Ok()", &mut env);
-        check_io_str("(cdr (car p))", "Ok(2)", &mut env);
-        check_io_str("(cdr p)", "Ok([3, 4])", &mut env);
-        check_io_str("p", "Ok([[1, 2], [3, 4]])", &mut env);
+        let env = &mut env;
+        check_io_str("(car (cons 123 456))", "Ok(123)", env);
+        check_io_str("(cdr (cons 123 456))", "Ok(456)", env);
+        check_io_str("(define p (cons (cons 1 2) (cons 3 4)))", "Ok()", env);
+        check_io_str("(cdr (car p))", "Ok(2)", env);
+        check_io_str("(cdr p)", "Ok([3, 4])", env);
+        check_io_str("p", "Ok([[1, 2], [3, 4]])", env);
         check_io_str(
             "(define l (cons 1 (cons 2 (cons 3 null))))",
             "Ok()",
-            &mut env,
+            env,
         );
-        check_io_str("(car (cdr l))", "Ok(2)", &mut env);
-        check_io_str("(cdr (cdr (cdr l)))", "Ok([])", &mut env);
+        check_io_str("(car (cdr l))", "Ok(2)", env);
+        check_io_str("(cdr (cdr (cdr l)))", "Ok([])", env);
     }
 
     #[test]
     fn test_fibonacci() {
         let mut env: Env = get_prelude();
+        let env = &mut env;
         check_io_str(
             "(define fib (lambda (n) (if (< n 2) 1 (+ (fib (- n 1)) (fib (- n 2))))))",
             "Ok()",
-            &mut env,
+            env,
         );
-        check_io_str("(fib 20)", "Ok(10946)", &mut env);
+        check_io_str("(fib 20)", "Ok(10946)", env);
         check_io_str(
             "(define range (lambda (a b) (if (= a b) (quote ()) (cons a (range (+ a 1) b)))))",
             "Ok()",
-            &mut env,
+            env,
         );
         check_io_str(
             "(define map (lambda (f l) (if (null? l) null (cons (f (car l)) (map f (cdr l))))))",
             "Ok()",
-            &mut env,
+            env,
         );
         check_io_str(
             "(range 0 10)",
             "Ok([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])",
-            &mut env,
+            env,
         );
-        // check_io_str("(map fib (range 0 20))", "Ok([3, 4])", &mut env);
+        // check_io_str("(map fib (range 0 20))", "Ok([3, 4])", env);
     }
 }
