9851183f74d7f555943d1bcdd2a45dc840ebe4e9
Rami3L <rami3l@outlook.com>
2020-01-24 15:35:58 +0800

    Optimized arg parsing with clap.rs

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,11 @@ mod types;
 
 use crate::parser::{InFile, Input};
 use crate::prelude::{get_prelude, make_env_ptr};
-use std::env;
+use clap::App;
 use std::process;
+
+#[macro_use]
+extern crate clap;
 
 #[macro_use]
 extern crate lazy_static;
@@ -17,13 +20,20 @@ static WELCOME_BANNER: &'static str = "Welcome to rusk, a simple Scheme interpre
 static STDLIB_PATH: &'static str = "./scheme/stdlib.rkt";
 
 fn main() {
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
     // println!("Hello, rusk!");
     println!("{}", WELCOME_BANNER);
-    let mut args = env::args();
     let global_env = make_env_ptr(get_prelude());
 
     // Interpret source file
     let read_source_file = |path: &str| {
+        let mut inport = InFile::new(path);
+        run::repl(&mut inport, &mut std::io::sink(), &global_env)
+    };
+
+    let read_source_file_verbose = |path: &str| {
         let mut inport = InFile::new(path);
         print!(".. Reading `{}`: ", inport.file_str);
         run::repl(&mut inport, &mut std::io::sink(), &global_env)
@@ -31,17 +41,26 @@ fn main() {
         println!("Done.");
     };
 
-    let res = {
-        // Load stdlib
-        read_source_file(STDLIB_PATH);
-
-        if let Some(path) = args.nth(1) {
-            read_source_file(&path);
-        };
-
-        // Start REPL Mode
+    // REPL mode
+    let run_repl = || {
         let mut inport = Input::new();
         run::repl(&mut inport, &mut std::io::stdout(), &global_env)
+    };
+
+    let res = {
+        // Load stdlib
+        read_source_file_verbose(STDLIB_PATH);
+
+        if let Some(path) = matches.value_of("INPUT") {
+            if matches.is_present("repl") {
+                read_source_file_verbose(&path);
+                run_repl()
+            } else {
+                read_source_file(&path)
+            }
+        } else {
+            run_repl()
+        }
     };
 
     if let Err(e) = res {
